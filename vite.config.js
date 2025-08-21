fec821c9cebf12cdc572d02861ace8d74d31333c
mingyu <vkans7526@naver.com>
2025-08-21 11:22:12 +0900

    up

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/Memo/",
   plugins: [react()],
   optimizeDeps: { include: ["react/jsx-runtime"] },
 });
